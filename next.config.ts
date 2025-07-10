54ebd1abca19eb774e3fe812cebe1b6c181e7b01
ItsIyaye <simplynathan2007@gmail.com>
2025-07-10 17:01:38 +0100

    Third Commit

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,15 @@ const nextConfig: NextConfig = {
 
 // next.config.js
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  swSrc: 'public/sw.ts', // use a TypeScript service worker
+  swSrc: 'public/sw.js', // use a TypeScript service worker
 });
+
 
 module.exports = withPWA({
   reactStrictMode: true,
